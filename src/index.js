d3337bbe972688116b47f9c7838db676006a1e85
iSagdiev <jay.iskander@gmail.com>
2020-10-11 19:09:06 +0500

    fix: task uploading.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (typeof matrix === 'undefined' || matrix === []) {
+        return [];
+    }
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+      let temArr = [];
+        for (let j = 0; j < matrix[i].length; j++) {
+          temArr.push(matrix[i][j]);
+        }
+        if (i%2 != 0) {
+          temArr = temArr.sort((a,b) => a - b);
+          temArr.reverse();
+          arr = arr.concat(temArr);
+    
+        } else 
+        arr = arr.concat(temArr.sort((a,b) => a - b))
+    } 
+    return(arr);
 }
